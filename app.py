d480714fcb7689407e929ebc46a8a25745810cf4
Devlee247 <inseo8070@naver.com>
2021-07-09 17:23:22 +0900

    add post

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-from flask import Flask  # 서버 구현을 위한 Flask 객체 import
-from flask_restx import Api, Resource  # Api 구현을 위한 Api 객체 import
+from flask import Flask, json, request
 
-app = Flask(__name__)  # Flask 객체 선언, 파라미터로 어플리케이션 패키지의 이름을 넣어줌.
-api = Api(app)  # Flask 객체에 Api 객체 등록
+app = Flask(__name__)
 
+@app.route('/')
+def root():
+    return 'welcome to flask'
 
-@api.route('/hello')  # 데코레이터 이용, '/hello' 경로에 클래스 등록
-class HelloWorld(Resource):
-    def get(self):  # GET 요청시 리턴 값에 해당 하는 dict를 JSON 형태로 반환
-        return {"hello": "world!"}
+@app.route('/data/get')
+def data_get():
+    return json.jsonify({'success': '성공'}), 200
 
-@app.route('/asd')
-def hello():
-    return '<h1>Hello, World!</h1>'
-
+@app.route('/post', methods=['POST'])
+def post():
+    result = request.get_json()
+    return json.jsonify({'success': result}), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
